61770a6a26e8bc2cd64be87206e9c10381d73285
macbotxxx <assanamamichael@gmail.com>
2021-11-16 22:55:23 +0100

    updated the django compress v2

--- config/settings/production.py
+++ config/settings/production.py
@@ -109,13 +109,13 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # django-compressor
 # ------------------------------------------------------------------------------
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_ENABLED
-COMPRESS_ENABLED = True
+COMPRESS_ENABLED = False
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_STORAGE
 COMPRESS_STORAGE = STATICFILES_STORAGE
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_URL
 COMPRESS_URL = STATIC_URL  # noqa F405
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_OFFLINE
-COMPRESS_OFFLINE = False  # Offline compression is required when using Whitenoise
+COMPRESS_OFFLINE = True  # Offline compression is required when using Whitenoise
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_FILTERS
 COMPRESS_FILTERS = {
     "css": [
