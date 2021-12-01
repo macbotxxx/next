abb5e517af8aa131dd70240d3fc40a814444136e
Michael Asa <assanamamichael@gmail.com>
2021-12-01 12:23:30 +0100

    wishcounter fix

--- wishlist/context_processors.py
+++ wishlist/context_processors.py
@@ -2,7 +2,7 @@ from .models import Wishlist
 
 
 def wishlist_counter(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         wish_count = Wishlist.objects.filter(user=request.user).count()
     else:
         wish_count = 0
